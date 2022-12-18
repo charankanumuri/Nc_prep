fa365dfcb34516d3d34f9f1a0506ef71036ea1a8
charankanumuri <charan.kanumuri@gmail.com>
2022-12-18 00:11:50 -0800

    NC 150 - 2 Pointers

---
+++ src/Two_Pointers/Trapping_Rain_Water.java
@@ -0,0 +1,50 @@
+package Two_Pointers;
+
+/* Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+ 
+
+Example 1:
+
+
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+Example 2:
+
+Input: height = [4,2,0,3,2,5]
+Output: 9
+ 
+
+Constraints:
+
+n == height.length
+1 <= n <= 2 * 104
+0 <= height[i] <= 105 */
+
+public class Trapping_Rain_Water {
+
+    public static int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int leftMax = height[left], rightMax = height[right];
+        int res = 0;
+        while (left < right) {
+            if (leftMax < rightMax) {
+                left += 1;
+                leftMax = Math.max(leftMax, height[left]);
+                res += leftMax - height[left];
+            } else {
+                right -= 1;
+                rightMax = Math.max(rightMax, height[right]);
+                res += rightMax - height[right];
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] height = { 4, 2, 0, 3, 2, 5 };
+        System.out.println(trap(height));
+    }
+
+}
